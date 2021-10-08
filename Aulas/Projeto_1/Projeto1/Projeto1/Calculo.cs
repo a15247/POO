32d278c7abcb44f2fd1b0c824d03d36bbc501f74
a15247 <a15247@alunos.ipca.pt>
2021-10-08 20:10:49 +0100

    First commit

---
+++ Aulas/Projeto_1/Projeto1/Projeto1/Calculo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto1
+{
+    class Calculo
+    {
+        static void mostrarArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        public static int MaiorValorArray(int[] array)
+        {
+            int ValorMaior = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > ValorMaior)
+                {
+                    ValorMaior = array[i];
+                }
+            }
+            return  ValorMaior;
+        }
+    }
+}
---
+++ Aulas/Projeto_1/Projeto1/Projeto1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Projeto1;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int [5]{ 1, 2, 3, 4, 5 };
+
+
+            Console.WriteLine(Calculo.MaiorValorArray);
+        }
+    }
+}
